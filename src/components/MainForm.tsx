36f4e90b057d728264daf2c557c7ebe170943b7a
ACE <hanan.a.masoud2003@gmail.com>
2024-09-10 21:17:23 +0300

    - some other things like (form validation , form submition , display errors ) - default values

--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -4,20 +4,96 @@ import {DevTool} from '@hookform/devtools'
 
 export const MainForm = () => {
 
-  const form = useForm()
-  const {register, control} = form
+  type FormValues = {
+    username: string,
+    email: string,
+    channel: string
+  }
+
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: 'Batman',
+      email: '',
+      channel: '',
+    }
+
+    //  From API
+    // async () => {
+    //   const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
+    //   const data = await response.json()
+    //   return {
+    //     username: 'Batman',
+    //     email: data?.email,
+    //     channel: ''
+    //   }
+    // }
+  })
+  const {register, control, handleSubmit, formState} = form
+
+  const {errors} = formState
+
+  const onSubmit = (data: FormValues) =>{
+    console.log('Form Submitted', data)
+  }
 
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <div className='form-control'>
             <label htmlFor="username">Username: </label>
-            <input type="text" id='username' {...register("username")}/>
+            <input 
+            type="text" 
+            id='username' 
+            {...register("username", 
+            {required :{
+              value:true,
+              message: 'Username is required',
+            }
+            })}/>
+            <p className='error'>{errors.username?.message}</p>
+            </div>
 
-            <label htmlFor="email">ُEmail: </label>
-            <input type="text" id='email' {...register("email")}/>
+            <div className='form-control'>
+            <label htmlFor="email">Email: </label>
+            <input 
+            type="text" 
+            id='email' 
+            {...register("email",
+              {pattern:{
+                value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: 'Invalid email format',
+              },
+              validate:{
+                notadmin: (fieldValue) => {
+                  return (fieldValue !== 'admin@gmail.com' || 
+                    'Enter a diffrent email address'
+                  )
+                },
+                notBlockListed: (fieldValue) => {
+                  return (
+                    !fieldValue.endsWith('baddomain.com') ||
+                    'This domain is not supported'
+                  )
+                }
+              }
+            }
+            )}/>
+            <p className='error'>{errors.email?.message}</p>
+            </div>
 
+            <div className='form-control'>
             <label htmlFor="channel">Channel: </label>
-            <input type="text" id='channel' {...register("channel")}/>
+            <input 
+            type="text" 
+            id='channel' 
+            {...register("channel", 
+            {required:{
+              value:true,
+            message:'Channel is required',
+            }
+            })}/>
+            <p className='error'>{errors.channel?.message}</p>
+            </div>
 
             <button>Submit</button>
         </form>
