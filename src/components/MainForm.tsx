81fd93de0192bf1ecbabc1b56c825486cc335516
ACE <hanan.a.masoud2003@gmail.com>
2024-09-16 09:33:16 +0300

    - Dynamic Field - Numeric and Date Values

--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
-import {DevTool} from '@hookform/devtools'
+import { useForm, useFieldArray } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
 
 export const MainForm = () => {
 
@@ -8,11 +8,16 @@ export const MainForm = () => {
     username: string,
     email: string,
     channel: string,
-    social:{
-      twitter : string,
-      facebook : string,
+    social: {
+      twitter: string,
+      facebook: string,
     },
-    phoneNumbers : string[]
+    phoneNumbers: string[],
+    phNumber: {
+      number: string
+    }[],
+    age: number,
+    dob: Date,
   }
 
   const form = useForm<FormValues>({
@@ -20,11 +25,14 @@ export const MainForm = () => {
       username: 'Batman',
       email: '',
       channel: '',
-      social:{
+      social: {
         twitter: '',
-        facebook:'',
+        facebook: '',
       },
-      phoneNumbers: ['' , '']
+      phoneNumbers: ['', ''],
+      phNumber: [{ number: '' }],
+      age: 0,
+      dob: new Date(),
     }
 
     //  From API
@@ -38,108 +46,173 @@ export const MainForm = () => {
     //   }
     // }
   })
-  const {register, control, handleSubmit, formState} = form
+  const { register, control, handleSubmit, formState } = form
 
-  const {errors} = formState
+  const { fields, append, remove } = useFieldArray({
+    name: 'phNumber',
+    control
+  })
 
-  const onSubmit = (data: FormValues) =>{
+  const { errors } = formState
+
+  const onSubmit = (data: FormValues) => {
     console.log('Form Submitted', data)
   }
 
   return (
     <div>
-        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-          <div className='form-control'>
-            <label htmlFor="username">Username: </label>
-            <input 
-            type="text" 
-            id='username' 
-            {...register("username", 
-            {required :{
-              value:true,
-              message: 'Username is required',
-            }
-            })}/>
-            <p className='error'>{errors.username?.message}</p>
-            </div>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className='form-control'>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id='username'
+            {...register("username",
+              {
+                required: {
+                  value: true,
+                  message: 'Username is required',
+                }
+              })} />
+          <p className='error'>{errors.username?.message}</p>
+        </div>
 
-            <div className='form-control'>
-            <label htmlFor="email">Email: </label>
-            <input 
-            type="text" 
-            id='email' 
+        <div className='form-control'>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            id='email'
             {...register("email",
-              {pattern:{
-                value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                message: 'Invalid email format',
-              },
-              validate:{
-                notadmin: (fieldValue) => {
-                  return (fieldValue !== 'admin@gmail.com' || 
-                    'Enter a diffrent email address'
-                  )
+              {
+                pattern: {
+                  value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: 'Invalid email format',
                 },
-                notBlockListed: (fieldValue) => {
-                  return (
-                    !fieldValue.endsWith('baddomain.com') ||
-                    'This domain is not supported'
-                  )
+                validate: {
+                  notadmin: (fieldValue) => {
+                    return (fieldValue !== 'admin@gmail.com' ||
+                      'Enter a diffrent email address'
+                    )
+                  },
+                  notBlockListed: (fieldValue) => {
+                    return (
+                      !fieldValue.endsWith('baddomain.com') ||
+                      'This domain is not supported'
+                    )
+                  }
                 }
               }
-            }
-            )}/>
-            <p className='error'>{errors.email?.message}</p>
-            </div>
+            )} />
+          <p className='error'>{errors.email?.message}</p>
+        </div>
 
-            <div className='form-control'>
-            <label htmlFor="channel">Channel: </label>
-            <input 
-            type="text" 
-            id='channel' 
-            {...register("channel", 
-            {required:{
-              value:true,
-            message:'Channel is required',
-            }
-            })}/>
-            <p className='error'>{errors.channel?.message}</p>
-            </div>
+        <div className='form-control'>
+          <label htmlFor="channel">Channel: </label>
+          <input
+            type="text"
+            id='channel'
+            {...register("channel",
+              {
+                required: {
+                  value: true,
+                  message: 'Channel is required',
+                }
+              })} />
+          <p className='error'>{errors.channel?.message}</p>
+        </div>
 
-            <div className='form-control'>
-            <label htmlFor="twitter">Twitter: </label>
-            <input 
-            type="text" 
-            id='twitter' 
-            {...register("social.twitter")}/>
-            </div>
+        <div className='form-control'>
+          <label htmlFor="age">Age: </label>
+          <input
+            type="text"
+            id='age'
+            {...register("age",
+              {
+                valueAsNumber: true,
+                required: {
+                  value: true,
+                  message: 'Age is required',
+                }
+              })} />
+          <p className='error'>{errors.age?.message}</p>
+        </div>
 
-            <div className='form-control'>
-            <label htmlFor="facebook">FaceBook: </label>
-            <input 
-            type="text" 
-            id='facebook' 
-            {...register("social.facebook")}/>
-            </div>
+        <div className='form-control'>
+          <label htmlFor="dob">Date of birth: </label>
+          <input
+            type="text"
+            id='dob'
+            {...register("dob",
+              {
+                valueAsDate: true,
+                required: {
+                  value: true,
+                  message: 'Date of birth is required',
+                }
+              })} />
+          <p className='error'>{errors.dob?.message}</p>
+        </div>
 
-            <div className='form-control'>
-            <label htmlFor="primary-phone">Primary phone number: </label>
-            <input 
-            type="text" 
-            id='primary-phone' 
-            {...register("phoneNumbers.0")}/>
-            </div>
 
-            <div className='form-control'>
-            <label htmlFor="secondary-phone">Secondary phone number: </label>
-            <input
+
+        <div className='form-control'>
+          <label htmlFor="twitter">Twitter: </label>
+          <input
+            type="text"
+            id='twitter'
+            {...register("social.twitter")} />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor="facebook">FaceBook: </label>
+          <input
+            type="text"
+            id='facebook'
+            {...register("social.facebook")} />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor="primary-phone">Primary phone number: </label>
+          <input
+            type="text"
+            id='primary-phone'
+            {...register("phoneNumbers.0")} />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor="secondary-phone">Secondary phone number: </label>
+          <input
             type="text"
             id='secondary-phone'
-            {...register("phoneNumbers.1")}/>
-            </div>
+            {...register("phoneNumbers.1")} />
+        </div>
 
-            <button>Submit</button>
-        </form>
-        <DevTool control={control}/>
+        <div>
+          <label> List of phone numbers</label>
+          <div>
+            {fields.map((field, index) => {
+              return (
+                <div className='form-control' key={field.id}>
+                  <input type="text"
+                    {...register(`phNumber.${index}.number` as const)}
+                  />
+                  {index > 0 && (
+                    <button type='button' onClick={() => remove(index)}>
+                      Remove
+                    </button>
+                  )}
+                </div>
+              )
+            })}
+            <button type='button' onClick={() => append({ number: '' })}>
+              Add phone number
+            </button>
+          </div>
+        </div>
+
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
     </div>
   )
 }
